4f54c45dea6927a3777bee887123484a82567da2
bordem <martin.borde19@gmail.com>
2018-01-25 12:42:32 +0100

    Premier commit

---
+++ Page1.php
@@ -0,0 +1,6 @@
+<html>
+    <form method="post" action="connexion.php">
+        <input type="texte" name="pseudo" />
+        <input type="submit" value="Connexion" />
+    </form>
+</html>
---
+++ connexion.php
@@ -0,0 +1,5 @@
+<?php
+
+    echo "Bonjour ".$_POST['pseudo']." !"
+
+?>
