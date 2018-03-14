5b78e462e234c97a360bf2b1482e1fd116c66ca3
bordem <martin.borde19@gmail.com>
2018-03-14 11:06:47 +0100

    <BORDE>

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Accueil du site</title>
+  </head>
+  <body>
+		<?php include('header.html');?>
+        <main>
+			<article>
+				<h2>Venez découvrir notre grande sélection de produit</h2>
+				<a href="fromages.php"><img src="img/fromageDecouverte.jpg"></img></a>
+			</article>
+			<article>
+				<h2>Deja client? Venez vous inscrire !</h2>
+				<a href="connexion.php"><img src="img/fidelisation.jpg"></img></a>
+			</article>
+        </main>
+		<?php include ('footer.html')?>
+  </body>
+</html>
+
