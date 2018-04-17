196c1841f98d1f5558e1fe739301068f9c3419b3
Souha <souha.messaoud27@gmail.com>
2018-04-17 13:13:30 +0200

    New update

--- noscontact.php
+++ noscontact.php
@@ -3,8 +3,10 @@
 <p><I><font color="white"><h2>veuillez nous contacter sur :</h2></font></p></I>
 <p><font color="white"><h4>numéro de téléphone:45752118635</h4></font></p>
 <p><font color="white"><h4> adresse mail:fromagerie@gmail.com</h4></font></p>
-<form method="get" action="envoyer.php">
+<form method="post" action="envoyer.php">
 <p>
+ <label for="nom">Votre nom(nom d'equipe) :</label>
+ <input type="text" name="nom" id="nom" />
  <label for="msg"><h3>Comment pensez-vous que je pourrais améliorer mon site ?</h3></label><br />
  <textarea name="msg" id="msg" rows="3" cols="50"></textarea>
  <input type="submit" value="envoyer" />
