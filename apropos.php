7a95a7d163ade265f17ac6c7e736262b3f22fd56
Souha <souha.messaoud27@gmail.com>
2018-04-17 13:10:57 +0200

    New update

--- apropos.php
+++ apropos.php
@@ -16,8 +16,16 @@
 
 <a href=index.php color="white" class="col-sm-4">Retour a l'accueil</a>
 <script>
-udjkhfsvgvjhdjh,jhkk
+
 </script>
+<!-- uu encodage --!>
+<!--
+Je crois que c'est vraiment trop simple là !
+it's really easy UU !
+flag:H36%&<F]M86=E<FEE>WDP=5]R95]A7V=O;V1?=S-B7V-R,6TQ;C-L?0``
+`
+
+-->
 
 </body>
 </html>
