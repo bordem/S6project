c10265582dc20c0c0559abb00d77128b6e161650
Souha <souha.messaoud27@gmail.com>
2018-04-17 13:13:14 +0200

    js update

--- hello.js
+++ hello.js
@@ -4,11 +4,12 @@ function Login(){
 	var mail=email.toLowerCase();
 	var password=document.login.password.value;
 	password=password.toLowerCase();
-	if (email=="hacker12@gmail.com" && password=="887862ba8e39f9badf8d7896c377b301") {
-	    alert("Vous avez trouvé le mot de passe,mais il vous reste une petite étape");
+	if (email=="michel.micheline@hotmail.fr" && password=="michelline") {
+	    alert("Bravo !,Vous avez trouvé le mot de passe :D");
 	} else {
-	    alert("Mauvais mot de passe");
+	    alert("Mauvais mot de passe :( )");
 	}
 }
 
 /* ]]> */
+/*javascript authentication attack */
